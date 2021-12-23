cf91c127c2959832ad89b2c716bcfc812918714b
Thomas Mooij <2021747@talnet.nl>
2021-12-23 12:14:04 +0100

    php2 taak2 af

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,10 @@
+-- <?php
+$persoon["voornaam"]   = "Thomas";
+$persoon["achternaam"] = "Mooij";
+$persoon["opleiding"]  = "ict";
+$persoon["leeftijd"]   = 25;
+
+
+echo $persoon["voornaam"]." ".$persoon["achternaam"]." is ".$persoon["leeftijd"]." en zit op de opleiding ".$persoon["opleiding"];
+
+?>  
