c291b595e38d2cc31606996db4c8594a27426525
Thomas Mooij <2021747@talnet.nl>
2021-12-23 13:42:21 +0100

    php2 for loops af

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+echo "<ul>";
+for($x=0; $x<18; $x++){
+    echo "<li> ik ben.$x.jaar oud dus ik mag nog niet stemmen<li>";
+    
+};
+echo"<br>";
+echo "Ik ben 18 jaar oud dus ik mag stemmen";
+echo"</ul>";
+?>
+</body>
+</html>
+
+
+
