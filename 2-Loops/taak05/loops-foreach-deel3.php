ad4674dea9215f742ed03ffe32d78aa1fec13269
Thomas Mooij <2021747@talnet.nl>
2022-01-10 14:49:01 +0100

    foreach3 af

---
+++ 2-Loops/taak05/loops-foreach-deel3.php
@@ -0,0 +1,14 @@
+<?php
+$student  = array(
+    "voornaam"    => "Jan",
+    "achternaam"  => "Janssen",
+    "klas"        => "9A",
+    "Leeftijd"    => 17,
+    "Woonplaats"  => "Amstelveen"
+  );
+
+  foreach($student as $key => $value){
+    echo "$key <br> $value";
+  }
+
+  ?>
