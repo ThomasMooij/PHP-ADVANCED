e8d70e1db150866e93777763372e3b5207300c16
Thomas Mooij <2021747@talnet.nl>
2022-01-10 13:21:14 +0100

    foreach loops af

--- 2-Loops/taak02/loops-for.php
+++ 2-Loops/taak02/loops-for.php
@@ -14,7 +14,7 @@ for($x=0; $x<18; $x++){
     echo "<li> ik ben.$x.jaar oud dus ik mag nog niet stemmen<li>";
     
 };
-echo"<br>";
+// echo"<br>";
 echo "Ik ben 18 jaar oud dus ik mag stemmen";
 echo"</ul>";
 ?>
