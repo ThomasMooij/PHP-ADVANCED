aea442126d48b2eafb162c85b4d45a9328c83343
Thomas Mooij <2021747@talnet.nl>
2022-01-12 11:36:27 +0100

    taak 01 while loop verbeterd

--- 2-Loops/taak01/loops-while.php
+++ 2-Loops/taak01/loops-while.php
@@ -1,7 +1,9 @@
 <?php
 $leeftijd=0;
-while($leeftijd <= 18){
+while($leeftijd < 18){
    echo "<ul> <li> ik ben ".$leeftijd." "." jaar oud dus ik mag nog niet stemmen </li> </ul>";
    $leeftijd ++;
 }
+echo "<ul> <li> ik ben ".$leeftijd." "." jaar oud dus ik mag stemmen </li> </ul>";
+
 ?>
