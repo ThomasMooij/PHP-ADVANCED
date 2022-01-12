a65ba17ed793377dd73d5cb8af3efde345422f58
Thomas Mooij <2021747@talnet.nl>
2022-01-12 11:38:42 +0100

    taak 2 array verbeterd

--- 1-Arrays/taak02/index.php
+++ 1-Arrays/taak02/index.php
@@ -1,8 +1,8 @@
 -- <?php
-$persoon["voornaam"]   = "Thomas";
-$persoon["achternaam"] = "Mooij";
-$persoon["opleiding"]  = "ict";
-$persoon["leeftijd"]   = 25;
+$persoon["voornaam"]   = "Willem Alexander";
+$persoon["achternaam"] = "Van Oranje";
+$persoon["opleiding"]  = "Software Ontwikkeling.";
+$persoon["leeftijd"]   = 53;
 
 
 echo $persoon["voornaam"]." ".$persoon["achternaam"]." is ".$persoon["leeftijd"]." en zit op de opleiding ".$persoon["opleiding"];
