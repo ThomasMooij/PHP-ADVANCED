51cb17c88c8c5c49f983f9761599360e21b780e9
Thomas Mooij <2021747@talnet.nl>
2022-01-12 12:14:12 +0100

    From taak 01 af

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo $_POST["Voornaam"]." ".$_POST["Achternaam"]."<br>"." zit in klas ".$_POST["Klas"]." hij is ".$_POST["Leeftijd"]." Jaar oud"."<br>";
+    echo "Hij woont op het ".$_POST["Adres"]." te ".$_POST["Plaatsnaam"]." en zijn favoriete band is ".$_POST["FavorieteBand"];
+    ?>
+</body>
+</html>
