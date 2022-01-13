02a7785c5d5ba5753ceeb9a336c793bbc0b4a4cb
Thomas Mooij <2021747@talnet.nl>
2022-01-13 13:44:12 +0100

    muziek opdracht af

---
+++ 3-Forms/taak03/muziek.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form name="form" method="GET" action="verstuur.php">
+    <label>Artiest:</label>
+    <input name="artist">
+    <label>Titel: </label>
+    <input name="titel">
+    <label>Jaar van uitgave: </label>
+    <input name="jaar">
+    <label>Genre:</label>
+    <input name="genre">
+    <button>Verzend</button>
+</form>
+</body>
+</html>
---
+++ 3-Forms/taak03/verstuur.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo $_GET["artist"];
+    echo $_GET["titel"];
+    echo $_GET["jaar"];
+    echo $_GET["genre"];
+    
+    
+    ?>
+    <!-- http://localhost/blok%204/PHP-ADVANCED/3-Forms/taak03/nummer.php?artist=brad+pit&titel=umbrella&year=0&genre=Punk -->
+</body>
+</html>
